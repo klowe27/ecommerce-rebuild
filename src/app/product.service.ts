ed1823d8a471e27f90c7101021309cdf9d50f770
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-25 13:06:16 -0800

    Add product service and display products in shop

---
+++ src/app/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+import { PRODUCTS } from './mock-products';
+
+@Injectable()
+export class ProductService {
+
+  constructor() { }
+
+  getProducts() {
+    return PRODUCTS;
+  }
+
+  getProductByID(productId: number){
+    for (var i = 0; i < PRODUCTS.length; i++) {
+      if (PRODUCTS[i].id === productId) {
+        return PRODUCTS[i];
+      }
+    }
+  }
+}
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model'
+import { Router } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
+  providers: [ProductService]
+
 })
 export class ShopComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit() {
+    this.products = this.productService.getProducts();
   }
+
+  goToProductPage(clickedProduct: Product){
+    this.router.navigate(['products', clickedProduct.id]);
+  }
+
+}
+
+
+
 
 }
