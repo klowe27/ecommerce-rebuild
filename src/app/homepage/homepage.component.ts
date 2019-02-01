aa02f42d765ef3d576ba1b459c231d01ee5b993c
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-01 12:08:13 -0800

    Add basic structure for homepage gallery

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  slideIndex:number = 1;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+// Next/previous controls
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+// Thumbnail image controls
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].style.display = "block";
+    dots[this.slideIndex-1].className += " active";
   }
 
 }
