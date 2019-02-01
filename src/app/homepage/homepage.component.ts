7d98f1f1bdbb5b05274b00c43fb6241c518d0c75
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-01 12:36:35 -0800

    Add logic for homepage gallery

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  slideIndex:number = 1;
+  slideIndex: number = 1;
 
   constructor() {
   }
@@ -15,27 +15,12 @@ export class HomepageComponent implements OnInit {
   }
 
   plusSlides(n) {
-    this.showSlides(this.slideIndex += n);
-  }
-
-  currentSlide(n) {
-    this.showSlides(this.slideIndex = n);
-  }
-
-  showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {this.slideIndex = 1}
-    if (n < 1) {this.slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    if (this.slideIndex === 3 && n === 1) {
+      this.slideIndex = 1;
+    } else if (this.slideIndex === 1 && n === -1) {
+      this.slideIndex = 3;
+    } else {
+      this.slideIndex+= n;
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[this.slideIndex-1].style.display = "block";
-    dots[this.slideIndex-1].className += " active";
   }
-
 }
