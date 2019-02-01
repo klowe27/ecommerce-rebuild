1579b91764942e616192ae2a4ce218ef6de90270
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-01-31 21:43:50 -0800

    Add Firebase database

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.model';
-import { PRODUCTS } from './mock-products';
+// import { PRODUCTS } from './mock-products';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Injectable()
 export class ProductService {
+  products: FirebaseListObservable<any[]>;
 
-  constructor() { }
-
-  getProducts() {
-    return PRODUCTS;
+  constructor(private database: AngularFireDatabase) {
+    this.products = database.list('products');
   }
 
-  getProductById(productId: number){
-    for (var i = 0; i < PRODUCTS.length; i++) {
-      if (PRODUCTS[i].id === productId) {
-        return PRODUCTS[i];
-      }
-    }
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(productId: string){
+    return this.database.object('products/' + productId);
   }
 }
