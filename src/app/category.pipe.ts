597f8f4f289d0732b981a7496540153c3d9580e3
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-01 14:23:19 -0800

    Fix category filter pipe

--- src/app/category.pipe.ts
+++ src/app/category.pipe.ts
@@ -1,6 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import { Product } from './product.model';
-import { ProductService } from './product.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Pipe({
   name: "category",
@@ -9,25 +10,25 @@ import { ProductService } from './product.service';
 
 export class CategoryPipe implements PipeTransform {
   transform(input: Product[], category){
-
     let output: Product[] = [];
-
-    if (category == "T-Shirt" || category == "Sweatshirt" || category == "Accessory" || category == "Tank") {
-      for (var i = 0; i < input.length; i++) {
-        if (input[i].category === category) {
-          output.push(input[i]);
+    if (input) {
+      if (category == "T-Shirt" || category == "Sweatshirt" || category == "Accessory" || category == "Tank") {
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].category === category) {
+            output.push(input[i]);
+          }
         }
-      }
-    } else if (category == "Sale") {
-      for (var i = 0; i < input.length; i++) {
-        if (input[i].sale === true) {
-          output.push(input[i]);
+      } else if (category == "Sale") {
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].sale === true) {
+            output.push(input[i]);
+          }
         }
-      }
-    } else {
-      for (var i = 0; i < input.length; i++) {
-        if (input[i].featured === true) {
-          output.push(input[i]);
+      } else {
+        for (var i = 0; i < input.length; i++) {
+          if (input[i].featured === true) {
+            output.push(input[i]);
+          }
         }
       }
     }
